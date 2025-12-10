0024fc7f561949b4a6de10c73a56b9bdde9b21d0
misterkante <gilchrist.kante@epitech.eu>
2025-12-10 16:20:27 +0100

    feat : test if changes are active on deploy domain

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,13 +29,7 @@ export default function AppHeader() {
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16 lg:h-20">
           {/* Logo avec image */}
-          <a href="/" className="flex items-center cursor-pointer">
-            <img 
-              src="../src/assets/logo-Mao.PNG" 
-              alt="MAO Logo" 
-              className="h-10 lg:h-14 w-auto object-contain"
-            />
-          </a>
+         KLOP
 
           {/* Desktop Navigation */}
           <nav className="hidden lg:flex items-center gap-8">
